72f78c3ff43365fc46da995365845f5785407003
Shilpika S <sshilpika5@gmail.com>
2016-02-09 19:43:10 -0600

    insertion sort test

--- insertion_sort/test/insertion_sort_test.py
+++ insertion_sort/test/insertion_sort_test.py
@@ -1,9 +1,16 @@
 import unittest
+import sys
+sys.path.append('../src')
 from insertion_sort import InsertionSort
+import random
 class Test(unittest.TestCase):
     def testInsertionSort(self):
-        A = [8, 5, 3, 1, 9, 6, 0, 7, 4, 2, 5]
+        # randomly generate 1000 random numbers in the range -100 to 100
+        A = [random.randint(-100,100) for c in range(1000)]
+
+        print("before sort",A)
         InsertionSort.sort(A)
+        print('After sort',A)
         for i in range(1, len(A)):
             if(A[i-1] > A[i]):
                 self.fail('insertion sort method fails')
